66e0bfc171b4f4576b753a9eefade32439a2c491
Adrian-Haba <56442732+Adrian-Haba@users.noreply.github.com>
2021-03-05 20:32:25 +0100

    Delete proyecto-entornocliente directory

--- proyecto-entornocliente/backend/src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-//Conexión con la base de datos
-mongoose.connect('mongodb://localhost/cajeroweb-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-    .then(db => console.log('Database is Connected'))
-    .catch(err => console.log(err));
