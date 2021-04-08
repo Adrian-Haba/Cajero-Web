13701256b283ac80c049268e1cdf246807fbecd6
Adrian-Haba <adrian9sevillano@hotmail.com>
2021-04-08 20:49:57 +0200

    # Implementación nombre de usuario  #
    Salida del nombre del usuario logueado en la esquina superior izquierda en la ventana de "cuentas", completando la frase de: "Bienvenido, username"

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,99 +5,111 @@ const User = require('../models/User');
 
 const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res) => res.send('Hello world'))
+router.get('/', (req, res) => res.send("Hello World"));
 
 router.post('/register', async (req, res) => {
   //Guardamos al usuario dentro de la base de datos
-    const { email, password } = req.body;
-    const newUser = new User({email, password});
+    const { email, name, password } = req.body; 
+    const newUser = new User({email, name, password});
     await newUser.save();
   // Una vez guardado creamos un token
-    const token = jwt.sign({_id: newUser._id }, 'credenciales')
+    const token = jwt.sign({_id: newUser._id, name: newUser.name }, 'credenciales')
   // Devolvemos el token al usuario
-    res.status(200).json({token})
-
-})
+    res.status(200).json({token, email, name})
+});
 
 router.post('/login', async (req, res) => {
   //El usuario introduce sus credenciales que son buscados en la base de datos
   const {email, password } = req.body;
   const user = await User.findOne({email})
   if(!user) return res.status(401).send("El email no existe")
-  if(user.password !== password) return res.status(401).send("Contraseña incorrecta"); // SIN CIFRADO DE PASSWORD
+  if(user.password !== password) return res.status(401).send("Contraseña incorrecta");
 
-  const token = jwt.sign({_id: user._id}, 'credenciales');
+  const token = jwt.sign({_id: user._id, name: user.name}, 'credenciales');
   return res.status(200).json({token});
+  
 
-
-
-
+  
 });
 
-//Ruta pública para poder devolver datos
-router.get('/public-tasks', (req, res) =>{
+//EJEMPLO Ruta pública para poder devolver datos
+router.get('/tasks', (req, res) =>{
   res.json([
     {
-      _id: 1,
-      name: 'lorem ipsum',
-      description: 'dolor sit amet',
+      _id: '1',
+      name: "lorem ipsum",
+      description: 'lorem ipsum',
       date: "2021-03-04T11:02:43.765Z"
     },
     {
-      _id: 2,
-      name: 'Pepe flores',
-      description: 'EL PEPE',
+      _id: '2',
+      name: "lorem ipsum",
+      description: 'lorem ipsum',
       date: "2024-03-04T11:02:43.765Z"
     },
     {
-      _id: 3,
-      name: 'Lampara',
-      description: 'Lampara',
+      _id: '3',
+      name: "lorem ipsum",
+      description: 'lorem ipsum',
       date: "2021-03-04T17:02:43.765Z"
     }
   ])
 });
 
-//Ruta privada para devolver datos privados
+//EJEMPLO Ruta privada para devolver datos privados
 router.get('/private-tasks', verifyToken, (req, res) =>{
   res.json([
     {
-      _id: 1,
-      name: 'lorem ipsum',
-      description: 'dolor sit amet',
+      _id: '1',
+      name: "lorem ipsum",
+      description: 'lorem ipsum',
       date: "2021-03-04T11:02:43.765Z"
     },
     {
-      _id: 2,
-      name: 'Pepe flores',
-      description: 'EL PEPE',
+      _id: '2',
+      name: "lorem ipsum",
+      description: 'lorem ipsum',
       date: "2024-03-04T11:02:43.765Z"
     },
     {
-      _id: 3,
-      name: 'Lampara',
-      description: 'Lampara',
+      _id: '3',
+      name: "lorem ipsum",
+      description: 'lorem ipsum',
       date: "2021-03-04T17:02:43.765Z"
     }
   ])
 });
 
-module.exports = router;
+//Ruta privada para pedir el nombre del usuario logueado
+router.get('/username', verifyToken, (req, res) => {
+  var usernameLC = req.userName.toLowerCase()
+  res.json(usernameLC);
+});
 
 //Validación
-function verifyToken(req, res, next) {
+ function verifyToken(req, res, next) {
+  try {
   //Revisar si en cada petición existe un header llamado "authorization"
     if (!req.headers.authorization) {
       return res.status(401).send('Acceso denegado')
     }
     //Revisar que el token no esté vacío
-    const token = req.headers.authorization.split(' ')[1]
+    let token = req.headers.authorization.split(' ')[1]
     if(token === 'null') {
       return res.status(401).send('Acceso denegado')
     }
     /*Extraer los datos desde el token y lo guardamos en una propiedad llamada "req.userId", para que lo puedan utilizar
     las demás funciones*/
     const payload = jwt.verify(token, 'credenciales')
-    req.userId = payload._id;
+    if (!payload) {
+      return res.status(401).send('Acceso denegado');
+    } 
+    req.userName = payload.name;
     next();
+    
+  } catch(e) {
+    return res.status(401).send('Acceso denegado');
+  }
 }
+
+module.exports = router;
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
+  error: boolean = false;
+
   user = {
     email: '',
     password: ''
@@ -18,16 +20,26 @@ export class LoginComponent {
     private authService: AuthService,
     private router: Router
     ) { }
-
+  
   login() {
-    this.authService.login(this.user)
+    if(this.user.email == '') {
+      alert("El email está vacío")
+    }else if(this.user.password == '') {
+      alert("La contraseña está vacía")
+    }else{
+      this.authService.login(this.user)
       .subscribe(
         res => {
           console.log(res)
           localStorage.setItem('token', res.token);
+          alert(`Sesión iniciada correctamente. Bienvenido "${this.user.email}"`);
           this.router.navigate(['/accounts']);
         },
-        err => console.log(err)
+        err => {
+          return console.log(err),
+          alert("Usuario o contraseña incorrectos");
+        }
       )
+    }
   }
 }
